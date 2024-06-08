01d8aa6e04b3d7d61c7f8ab86dd1715388249a4a
Shad0wRim <jungouicha@gmail.com>
2024-06-07 23:49:25 -0500

    added threefold repetition and removed io

---
+++ src/counter.rs
@@ -0,0 +1,41 @@
+use std::{collections::HashMap, hash::Hash};
+
+#[derive(Debug)]
+pub struct Counter<Key: Hash + Eq> {
+    map: HashMap<Key, usize>,
+}
+
+#[allow(dead_code)]
+impl<Key: Hash + Eq> Counter<Key> {
+    pub fn new() -> Counter<Key> {
+        Counter {
+            map: HashMap::new(),
+        }
+    }
+    pub fn add(&mut self, key: Key) {
+        if let Some(count) = self.map.get_mut(&key) {
+            *count += 1;
+        } else {
+            self.map.insert(key, 1);
+        }
+    }
+    pub fn from(collection: impl IntoIterator<Item = Key>) -> Counter<Key> {
+        let mut counter = Counter::new();
+        for item in collection {
+            counter.add(item);
+        }
+        counter
+    }
+    pub fn counts(&self) -> impl Iterator<Item = &usize> {
+        self.map.values()
+    }
+    pub fn get(&self, key: &Key) -> Option<&usize> {
+        self.map.get(key)
+    }
+}
+
+impl<Key: Hash + Eq> Default for Counter<Key> {
+    fn default() -> Self {
+        Counter::new()
+    }
+}
